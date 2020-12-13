c82475ed935d211529e3b1d7e5491018c0c97698
DmitryShastel <dmshastel@gmail.com>
2020-12-13 11:38:40 +0300

    sicial_network 1-31

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import s from './../Dialogs.module.css';
 
 const Message = (props) => {
+
+    let newMessageElement = React.createRef();
+
+    let addMessage = () => {
+        let message = newMessageElement.current.value;
+        alert(message);
+    }
+
     return (
         <div className={s.dialog}>
             <img src='https://iconarchive.com/download/i75883/martz90/circle/messages.ico'/>
             {props.message}
+            <textarea ref={newMessageElement}></textarea>
+            <button onClick={addMessage}>add</button>
 
         </div>
     )
