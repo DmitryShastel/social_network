510bdc6bb86c86fd79ec72e58c1a341497d4dd6c
DmitryShastel <dmshastel@gmail.com>
2021-02-17 00:02:42 +0300

    social_network 1-49 Users_Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import {Route} from "react-router-dom";
 import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
 
 
 const App = (props) => {
@@ -20,6 +21,9 @@ const App = (props) => {
 
                 <Route path = '/profile/'
                        render = {() => <Profile/>}/>
+
+                <Route path = '/users/'
+                       render = {() => <UsersContainer/>}/>
             </div>
         </div>
     )
