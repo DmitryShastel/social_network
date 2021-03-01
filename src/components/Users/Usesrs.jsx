d5e3a5b32441a34fb0c5d612b4a71c39d2c9838c
DmitryShastel <dmshastel@gmail.com>
2021-03-01 13:11:54 +0300

    social_network 1-52 Side_Effect

--- src/components/Users/Usesrs.jsx
+++ src/components/Users/Usesrs.jsx
@@ -5,17 +5,22 @@ import userPhoto from '../../assets/images/user-logo.png'
 
 
 export let Users = (props) => {
-    if (props.users.length === 0) {
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            debugger;
-            props.setUsers(response.data.items);
-        });
+    let getUsers = () => {
+        if (props.users.length === 0) {
 
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                debugger;
+                props.setUsers(response.data.items);
+            });
+
+        }
     }
+
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {
                 props.users.map( u => <div key={u.id}>
                     <span>
