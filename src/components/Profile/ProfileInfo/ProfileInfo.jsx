5262745fc71210f1d71b7c8df37e1dbbaf6935ab
DmitryShastel <dmshastel@gmail.com>
2021-04-10 11:56:33 +0300

    social_network 1-59 profile,ajax,api

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import s from './ProfileInfo.module.css';
+import {Preloader} from "../../common/Preloader/Preloader";
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    debugger;
+    if (!props.profile) {
+        return <Preloader/>
+    }
+
     return (
         <div className={s.wrapper}>
             <div >
                 <img className={s.img}
                     src='https://cdn-cms.pgimgs.com/news/2015/07/Maldives-beach.jpg'/>
             </div>
-            <div className={s.descriptionBlock}>
+            <div className = {s.descriptionBlock}>
+                <img src={props.profile.photos.large}/>
                 ava + description
             </div>
         </div>
